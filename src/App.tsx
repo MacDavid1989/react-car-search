587778c997bc52b41c31470fb0d7679a715b0a0c
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-27 09:24:30 -0400

    set up getModelsForMake function to fetch data and create a type for the function

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,21 @@ import { Drawer, LinearProgress, Grid } from "@material-ui/core";
 // Styles
 import { Wrapper } from "./App.styles";
 
+// Types
+export type CarModelType = {
+  Make_ID: number;
+  Make_Name: string;
+  Model_ID: number;
+  Model_Name: string;
+};
+
+const getModelsForMake = async (make: string): Promise<CarModelType> =>
+  await (
+    await fetch(
+      `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMake/${make}?format=json`
+    )
+  ).json();
+
 const App = () => {
   return <div className="App">Start</div>;
 };
