e598cb0ff88b8e8f93b4ca011f0050bcfbe85459
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-29 09:34:52 -0400

    adjust button size and color

--- src/Styles/ComponentStyles/BackToTop.styles.ts
+++ src/Styles/ComponentStyles/BackToTop.styles.ts
@@ -4,21 +4,20 @@ export const Wrapper = styled.div`
   .back-to-top {
     position: fixed;
     bottom: 20px;
-    right: 20px;
-    height: 75px;
-    width: 75px;
-    font-size: 54px;
-    background: #95c93d;
-    color: #fff;
+    right: 40px;
+    height: 60px;
+    width: 60px;
+    font-size: 36px;
     cursor: pointer;
     border-radius: 100%;
     border: 1px solid #fff;
     display: flex;
     align-items: center;
     justify-content: center;
+    background: rgba(255, 255, 255, 0.3);
   }
 
   .back-to-top:hover {
-    background: #65b82d;
+    background: #95c93d;
   }
 `;
